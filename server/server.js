6cf514bd28accd46b2b5b0dd26838d043f7bf6c1
mohdriyaan <mohammedrayaan1@gmail.com>
2026-09-15 21:12:58 +0000

    feat : replace Vite favicon with GitHub activity icon and setup the host at port 5000

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 import express from "express"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
 import githubRouter from "./routes/githubRoutes.js"
 import { githubRateLimiter } from "./middleware/rateLimiter.js"
 
@@ -6,7 +9,13 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const clientDistPath = path.join(__dirname, "../client/dist")
+
 app.set("trust proxy", 1)
+
 app.use(express.json())
 
 app.get("/api/health", (req, res) => {
@@ -14,6 +23,12 @@ app.get("/api/health", (req, res) => {
 })
 
 app.use("/api/github", githubRateLimiter, githubRouter)
+
+app.use(express.static(clientDistPath))
+
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(clientDistPath, "index.html"))
+})
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started running on port ${PORT}`)
